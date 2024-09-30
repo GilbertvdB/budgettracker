db96db8bf76a00a7b1b3377334650c66387d5bd7
Gilbert B. <101508384+GilbertvdB@users.noreply.github.com>
2024-09-30 21:13:59 +0200

    Update show.blade.php

--- resources/views/expenses/show.blade.php
+++ resources/views/expenses/show.blade.php
@@ -31,7 +31,9 @@
                     </div>
                     <small>by {{ $expense->user->email }}</small>
                     <div class="flex justify-between pt-2">
-                        <img src="https://placehold.co/30" alt="receipt-icon">
+                        <a href="{{ url($expense->url ) }}" target="_blank">
+                            <img src="https://placehold.co/30" alt="receipt-icon">
+                        </a>
                         <span class="text-xl">{{ $expense->total }}</span>
                     </div>
                 </div>
@@ -42,37 +44,3 @@
 @endif
 </div>
 </x-app-layout>
-<script>
-    function expenseData() {
-        return {
-            selectedBudget: '', // Track the selected budget
-            expenses: [],       // Store the fetched expenses
-            loading: false,     // Track loading state
-
-             // Fetch expenses for the selected budget
-             async fetchExpenses() {
-                if (this.selectedBudget) {
-                    this.loading = true;
-
-                    try {
-                        const response = await fetch(`/expenses/${this.selectedBudget}`);
-                        const data = await response.json();
-                        this.expenses = data.expenses.map(expense => ({
-                            ...expense,
-                            updated_at: new Date(expense.updated_at) // Convert to JS Date object
-                        }));
-                    } catch (error) {
-                        console.error('Error fetching expenses:', error);
-                    }
-
-                    this.loading = false;
-                }
-            },
-
-            // Format the date similar to Blade's d F format
-            formatDate(date) {
-                return date.toLocaleDateString('en-US', { day: 'numeric', month: 'long' });
-            }
-        }
-    }
-</script>
